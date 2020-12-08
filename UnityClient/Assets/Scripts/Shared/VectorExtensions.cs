009e485a987e5eed561b1595033c8f381fd233d3
Max Burow <max.burow@outlook.com>
2020-12-08 16:56:14 +0100

    add Vector3.Slide()

--- UnityClient/Assets/Scripts/Shared/VectorExtensions.cs
+++ UnityClient/Assets/Scripts/Shared/VectorExtensions.cs
@@ -1,10 +1,9 @@
 ﻿using UnityEngine;
 public static class VectorExtensions {
-    public static Vector2 Perpendicular(this Vector2 v) {
-        return new Vector2(-v.y, v.x);
-    }
+    public static Vector2 Perpendicular(this Vector2 v) => new Vector2(-v.y, v.x);
 
-    public static Vector3 Bounce(this Vector3 d, Vector3 normal) {
-        return d - 2 * Vector3.Dot(d, normal) * normal;
-    }
+    public static Vector3 Bounce(this Vector3 v, Vector3 normal) => v - 2 * Vector3.Dot(v, normal) * normal;
+
+    public static Vector3 Slide(this Vector3 v, Vector3 normal) => v - normal * Vector3.Dot(v, normal);
+
 }
